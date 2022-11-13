1ed052c12baa94cad76564cb9e6cc05ffb9a1db1
Gongati Nandha Kishore <102871703+nandha434@users.noreply.github.com>
2022-11-13 11:46:02 +0530

    Add files via upload

---
+++ madlib/Golden.py
@@ -0,0 +1,21 @@
+def madlib():
+    King_name = input('name :')
+    Verb = input('verb :')
+    God_name = input('Name :')
+    Wisher = input('name :')
+    Metal = input('metal :')
+    food = input('foof_item :')
+
+    story=f"""  There once was a king named {King_name} who did a good deed for a Satyr. And he was then {Verb} a wish by {Wisher}, the god of {God_name}.
+
+    For his wish, {King_name} asked that whatever he touched would turn to {Metal}. Despite {Wisher}’ efforts to prevent it, {King_name} pleaded that this was a fantastic wish, and so, it was bestowed.
+
+    Excited about his newly-earned powers, {King_name} started touching all kinds of things, turning each item into pure {Metal}.
+
+    But soon, {King_name} became hungry. As he picked up a piece of {food}, he found he couldn’t eat it. It had turned to {Metal} in his hand.
+
+    Hungry, {King_name} groaned, “I’ll starve! Perhaps this was not such an excellent wish after all!”"""
+
+    print(story)
+
+madlib()
